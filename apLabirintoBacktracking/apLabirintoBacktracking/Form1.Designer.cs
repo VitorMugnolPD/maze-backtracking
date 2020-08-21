1b4f0cf2267b37cb30e7fa38430452ab0eb83026
Vitor Mugnol <vitormugnol10@gmail.com>
2020-08-21 19:21:43 -0300

    exibição do labirinto

--- apLabirintoBacktracking/apLabirintoBacktracking/Form1.Designer.cs
+++ apLabirintoBacktracking/apLabirintoBacktracking/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.btnAbrirArquivo.TabIndex = 1;
             this.btnAbrirArquivo.Text = "Abrir arquivo";
             this.btnAbrirArquivo.UseVisualStyleBackColor = true;
+            this.btnAbrirArquivo.Click += new System.EventHandler(this.btnAbrirArquivo_Click);
             // 
             // dgvCaminhos
             // 
--- apLabirintoBacktracking/apLabirintoBacktracking/Form1.cs
+++ apLabirintoBacktracking/apLabirintoBacktracking/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using back_track;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,37 @@ namespace apLabirintoBacktracking
 {
     public partial class Form1 : Form
     {
+        Maze labirinto;
+        char[,] labirintoMatriz;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAbrirArquivo_Click(object sender, EventArgs e)
+        {
+            if(dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                labirinto = new Maze(dlgAbrir.FileName);
+                exibirLabirinto();
+            }
+        }
+
+        private void exibirLabirinto()
+        {
+            labirintoMatriz = labirinto.getMaze();
+            int linhas = labirinto.getRows();
+            int colunas = labirinto.getColumns();
+            dgvLabirinto.RowCount = linhas;
+            dgvLabirinto.ColumnCount = colunas;
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int n = 0; n < colunas-1; n++)
+                {
+                    dgvLabirinto[n, i].Value = labirintoMatriz[i, n].ToString();
+                }
+            }
         }
     }
 }
